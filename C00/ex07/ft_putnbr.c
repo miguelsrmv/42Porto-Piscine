b455452c997b80913e1d91936826cb26ed47960f
Miguel De Sa  Rodrigues Moura Vieira <mde-sa--@c1r5s1.42porto.com>
2023-03-10 09:21:50 +0000

    Cleanup

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,31 +6,11 @@
 /*   By: mde-sa-- <mde-sa--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 12:59:24 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/03/09 13:09:08 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/03/10 09:10:57 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putnbr(int nb)
-{
-	int	cb;
-	int	counter;
-
-	cb = nb;
-	counter = 0;
-	while (cb > 10)
-	{
-		cb = cb / 10;
-		counter = counter + 1;
-	}
-	writedigit(cb);
-	if (counter > 0)
-	{
-		nb = nb - cb * poweroften(counter);
-		ft_putnbr(nb);
-	}
-}
 
 void	writedigit(int n)
 {
@@ -49,5 +29,25 @@ int	poweroften(int e)
 	else
 	{
 		return (10 * poweroften(e - 1));
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	int	cb;
+	int	counter;
+
+	cb = nb;
+	counter = 0;
+	while (cb > 10)
+	{
+		cb = cb / 10;
+		counter = counter + 1;
+	}
+	writedigit(cb);
+	if (counter > 0)
+	{
+		nb = nb - cb * poweroften(counter);
+		ft_putnbr(nb);
 	}
 }
