d3da729565ce99c9e689646cbf65dc8a5833551b
Miguel De Sa  Rodrigues Moura Vieira <mde-sa--@c1r7s1.42porto.com>
2023-03-16 18:32:56 +0000

    Yay

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: mde-sa-- <mde-sa--@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 12:59:24 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/03/15 17:26:21 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:32:39 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
+	if (nb == -2147483648)//// TERMINAR AQUI!!!
+		
 	if (nb < 0)
 	{
 		ft_putchar('-');
@@ -31,4 +33,31 @@ void	ft_putnbr(int nb)
 	}
 	if (nb < 10)
 		ft_putchar(nb + 48);
+}
+
+#include <stdio.h>
+
+int main (void)
+{
+	int a = 1;
+	int b = -1;
+	int c = 0;
+	int d = 10;
+	int e = -10;
+	int f = 2147483647;
+	int g = -2147483648;
+
+	ft_putnbr(a);
+	printf("\n");
+	ft_putnbr(b);
+	printf("\n");
+	ft_putnbr(c);
+	printf("\n");
+	ft_putnbr(d);
+	printf("\n");
+	ft_putnbr(e);
+	printf("\n");
+	ft_putnbr(f);
+	printf("\n");
+	ft_putnbr(g);
 }
--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: mde-sa-- <mde-sa--@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 17:36:18 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/03/15 22:52:25 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:17:38 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ int	base_check(char *base)
 	i = 0;
 	while (base[i])
 	{
-		if (base[i] == '+' || base[i] == '-' || base[i] == '\f' ||
-			base [i] == '\r' || base[i] == '\t' || base[i] == '\v' ||
-			base[i] == '\n' || base[i] == ' ')
+		if (base[i] == '+' || base[i] == '-' || base[i] == '\f'
+			|| base [i] == '\r' || base[i] == '\t' || base[i] == '\v'
+			|| base[i] == '\n' || base[i] == ' ')
 			return (0);
 		j = i + 1;
 		while (base[j])
@@ -48,74 +48,7 @@ int	base_check(char *base)
 	return (1);
 }
 
-int	array_to_int(char *numberarray, int index, char *base, int sign)
-{
-	long	number;
-
-	number = 0;
-	while (numberarray[index])
-	{
-		number *= 10;
-		number += numberarray[index] - is_alphanum(&numberarray[index]);
-		index--;
-	}
-	return convert_to_base(number, base, sign);
-}
-
-
-int	convert_to_base(long nbr, char *original_base, int sign)
-{
-	int	number;
-	int	r;
-	int	power;
-	int	base;
-
-	number = 0;
-	power = 1;
-	base = ft_strlen(original_base);
-	if (!base_check(original_base))
-		return 0;
-	if (sign> 0)
-	{
-		while (nbr > 0)
-		{
-			r = nbr % 10;
-			number += r * power;
-			power *= base;
-			nbr /= 10;
-		}
-		return (number);
-	}
-	return (-convert_to_base(-nbr, original_base, 1));
-}
-
-
-/*
-int	ft_putnb_base(int nbr, char *base)
-{
-	long	numberarray[32];
-	int		index;
-	long	nb;
-	int		base_length;
-
-	nb = nbr;
-	base_length = ft_strlen(base);
-	if (!base_check(base))
-		return 0;
-	if (nb < 0)
-		nb = -nb;
-	index = 0;
-	while (nb != 0)
-	{
-		numberarray[index] = base[nb % base_length];
-		nb = nb / base_length;
-		index++;
-	}
-	return (array_to_int(numberarray, index-1));
-}
-*/
-
-int	is_alphanum(char *a)
+int	ascii_table(char *a)
 {
 	if (*a >= 'A' && *a <= 'Z')
 		return (65 - 10);
@@ -126,111 +59,53 @@ int	is_alphanum(char *a)
 	return (0);
 }
 
-/*
-int	ft_atoi_base(char *str, char *base)
+int	convert_number(char *numberarray, char *base, int sign)
 {
-	int		starting_index;
-	int		ending_index;
-	long	number;
+	int	i;
+	int	number;
+	int	len_numberarray;
+	int	len_base;
 
+	if (base_check(base) == 0)
+		return (0);
+	len_numberarray = ft_strlen(numberarray);
+	len_base = ft_strlen(base);
 	number = 0;
-	starting_index = 0;
-	while (is_alphanum(&str[starting_index]) == 0)
-		starting_index++;
-	ending_index = starting_index;
-	while (is_alphanum(&str[ending_index]) != 0)
-		ending_index++;
-	while (starting_index < ending_index)
+	i = 0;
+	while (i < len_numberarray)
 	{
-		number *= 10;
-		number += (str[starting_index]) - is_alphanum(&str[starting_index]);
-		starting_index++;
+		number *= len_base;
+		number += numberarray[i] - ascii_table(&numberarray[i]);
+		i++;
 	}
-	while (starting_index > 0)
-	{
-		if (str[starting_index] == 45)
-			number *= -1;
-		starting_index--;
-	}
-	return (convert_to_base(number, base));
-} */
-
+	number = number * sign;
+	return (number);
+}
 
 int	ft_atoi_base(char *str, char *base)
 {
 	int		starting_index;
-	int		i;
-	char	*number;
 	int		sign;
+	int		i;
+	char	number[10];
+	int		result;
 
 	starting_index = 0;
-	while (is_alphanum(&str[starting_index]) == 0)
+	sign = 1;
+	while (ascii_table(&str[starting_index]) == 0)
+	{
+		if (str[starting_index] == '-')
+			sign *= -1;
 		starting_index++;
+	}
 	i = 0;
-	while (is_alphanum(&str[starting_index]) != 0)
+	while (ascii_table(&str[starting_index]) != 0)
 	{
 		number[i] = (str[starting_index]);
 		i++;
 		starting_index++;
 	}
-	sign = 1;
-	while (starting_index > 0)
-	{
-		if (str[starting_index] == 45)
-			sign *= -1;
-		starting_index--;
-	}
-	return (convert_to_base(number, base, sign));
-} 
-
-void	ft_atoi_base_test(char *str, int i; char *base, int expected_number)
-{
-	int ft_buff;
-
-	ft_buff = ft_atoi_base(str, base);
-	if (ft_buff != expected_number)
-		printf("> KO, expected: %d got: %d\n", expected_number, ft_buff);
-	else
-		printf("> OK, result: %d\n", ft_buff);
+	number[i] = '\0';
+	result = (convert_number(number, base, sign));
+	return (result);
 }
-
-int	main(void)
-{
-	// teste com bases binarias
-	printf("Bases binarias:\n");
-	ft_atoi_base_test("\n \t\r \v \f++++--1000,.fs", "01", 8);
-//	ft_atoi_base_test("\n \t\r \v \f++++--*/*/**,.fs", "/*", 43);
-	// teste com bases octais
-	printf("Bases octais:\n");
-	ft_atoi_base_test("\n \t\r \v \f++-++--205,.fs", "01234567", -133);
-//	ft_atoi_base_test("\n \t\r \v \f+-+++--fdd,.fs", "abcdefgh", -347);
-	// teste com bases decimais
-	printf("Bases decimais:\n");
-	ft_atoi_base_test("\n \t\r \v \f++-++--2147483648,.fs", "0123456789", -2147483648);
-//	ft_atoi_base_test("\n \t\r \v \f+-+++-+jjf,.fs", "abcdefghij", 995);
-	// teste com bases hexadecimais
-	printf("Bases hexadecimais:\n");
-	ft_atoi_base_test("\n \t\r \v \f++-+-+--F0FA,.fs", "0123456789ABCDEF", 61690);
-//	ft_atoi_base_test("\n \t\r \v \f+-+++-+ninc,.fs", "abcdefghijklmnop", 55506);
-	// teste com bases invalidas
-	printf("Bases invalidas:\n");
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123456678", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123\n", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123\v", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123\t", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123\f", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123\r", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123 ", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123+", 0);
-	ft_atoi_base_test("\n \t\r ++-++-123456,das", "0123-", 0);
-}
-
-
-
-// Primeiro pegar na string e colocá-la numa array
-// Inverter a array (?)
-// Transformar a array num INT (como fazer com chars??)
-// Transformar em base 2
-// Return
