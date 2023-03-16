2b7425a3ae8160f6a63ddd87d340284427286968
Miguel De Sa  Rodrigues Moura Vieira <mde-sa--@c1r6s1.42porto.com>
2023-03-16 15:38:53 +0000

    OK

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -3,69 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: mde-sa-- <mde-sa--@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 11:32:08 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/03/16 12:11:32 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/03/16 12:54:34 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char	*ft_strstr(char *str, char *to_find);
-
-int	main(void)
-{
-	char *str;
-	char *to_find;
-	char *buff;
-	char *ft_buff;
-
-	str = strcpy(calloc(11, sizeof(char)), "alo galera");
-	to_find = strcpy(calloc(7, sizeof(char)), "galera");
-	buff = strstr(str, to_find);
-	ft_buff = ft_strstr(str, to_find);
-	if (buff != ft_buff)
-		printf("[1] KO, substrings don't match(expected:%s, got:%s)\n", buff, ft_buff); 
-	else
-		printf("[1] OK. expected: %s result: %s\n", buff, ft_buff);
-	str = strcpy(calloc(11, sizeof(char)), "camone");
-	to_find = strcpy(calloc(7, sizeof(char)), "cam.");
-	buff = strstr(str, to_find);
-	ft_buff = ft_strstr(str, to_find);
-	if (buff != ft_buff)
-		printf("[2] KO, substrings don't match(expected:%s, got:%s)\n", buff, ft_buff); 
-	else
-		printf("[2] OK. expected: %s result: %s\n", buff, ft_buff);
-	str = strcpy(calloc(11, sizeof(char)), "alo galera");
-	to_find = strcpy(calloc(1, sizeof(char)), "");
-	buff = strstr(str, to_find);
-	ft_buff = ft_strstr(str, to_find);
-	if (buff != ft_buff)
-		printf("[2] KO, substrings don't match(expected:%s, got:%s)\n", buff, ft_buff); 
-	else
-		printf("[2] OK. expected: %s result: %s\n", buff, ft_buff);
-	return (0);
-}
-
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	while ((s1[i] == s2[i]) && s1[i] && i < n)
+	while ((s1[i] == s2[i]) && s1[i] && i < (n - 1))
 		i++;
-	return ((unsigned char)s1[i - 1] - (unsigned char)s2[i - 1]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int		i;
 	int		to_find_size;
-
+	int		test;
+	
 	i = 0;
 	to_find_size = 0;
 	while (to_find[to_find_size])
@@ -76,7 +38,7 @@ char	*ft_strstr(char *str, char *to_find)
 	{
 		if (str[i] == to_find[0])
 		{
-			if (ft_strncmp(&str[i], to_find, to_find_size) == 0)
+			if (ft_strncmp(&str[i], to_find, to_find_size) == 0);
 				return (&str[i]);
 		}
 		i++;
--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: mde-sa-- <mde-sa--@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 12:28:04 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/03/15 13:14:16 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/03/16 15:36:41 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,48 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 
 	src_size = ft_strlen(src);
 	dest_size = ft_strlen(dest);
+
+	if (size <= dest_size)
+		return (src_size + size);
 	src_i = 0;
 	dest_i = dest_size;
 	while (src[src_i] && (src_i + dest_size) < size - 1)
 	{
-		dest[dest_i] = src[src_i];
+		dest[dest_i] = src[src_i];				
 		dest_i++;
 		src_i++;
 	}
 	dest[dest_i] = '\0';
-	return ((unsigned int)(dest_size + src_size));
+	return (dest_size + ft_strlen(&src[src_i]));
 }
+
+
+
+
+#include <stdio.h>
+
+int main (void)
+{
+	char source[7] = {'C', 'A', 'O', 'A', 'B', 'C', '\0'}; 
+	int size = 6;
+	char dest[6];
+	ft_strlcat(dest, source, size);
+
+	int i = 0;
+	while (i < size)
+	{
+		printf("%c,", dest[i]);
+		i++;
+	}
+		
+}
+
+/* 
+1.Take the full of the size of the buffer and guarantee to nul/terminate
+result if size > 0 or if there is one free byte in dest.
+2. A byte for null should be included in size
+3. src MUST be nul-terminated
+4. Copies up to size - 1 character
+5. Return total length of src
+6. Se nao encontrar nul apos size characters, o comprimento da string e size e dest
+NAO tem nul */
