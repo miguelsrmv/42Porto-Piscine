60ef4ae102e66444692a7d505a8d04be7507df75
Miguel De Sa  Rodrigues Moura Vieira <mde-sa--@c1r4s1.42porto.com>
2023-03-25 22:54:41 +0000

    Damas

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -6,34 +6,32 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 18:02:29 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/03/25 20:28:34 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/03/25 22:52:57 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-//Array[collumn] = linha que ocupa em dada coluna
-
-int position_valid (int collumn, int *array)
+int	position_valid(int collumn, int *array)
 {
-	int pos_check;
+	int	pos_check;
 
 	pos_check = 0;
-	while (pos_check < collumn)															// Posicoes horizontais
+	while (pos_check < collumn)
 	{
 		if (array[collumn] == array[pos_check])
 			return (0);
 		pos_check++;
 	}
 	pos_check = 1;
-	while (pos_check <= collumn && collumn - pos_check >= 0)							// Posicoes diagonais para cima
+	while (pos_check <= collumn && array[collumn] - pos_check >= 0)
 	{
 		if (array[collumn - pos_check] == array[collumn] - pos_check)
 			return (0);
 		pos_check++;
 	}
 	pos_check = 1;
-	while (pos_check <= collumn && collumn + pos_check < 10)							// Posicoes diagonais para baixo
+	while (pos_check <= collumn && array[collumn] + pos_check < 10)
 	{
 		if (array[collumn - pos_check] == array[collumn] + pos_check)
 			return (0);
@@ -42,65 +40,82 @@ int position_valid (int collumn, int *array)
 	return (1);
 }
 
-void success (int *array, int *counter)
+void	success(int *array, int *counter)
 {
-	int i;
+	int		i;
+	char	c;
 
 	i = 0;
-	while (array[i])
+	while (i < 10)
 	{
-		write(1, &array[i], 1);
-		write(1, "\n", 1);
+		c = array[i] + '0';
+		write (1, &c, 1);
 		i++;
 	}
+	write (1, "\n", 1);
 	(*counter)++;
 	return ;
 }
 
+int	end_of_collumn(int collumn, int *array)
+{
+	if (array[collumn] == 9)
+	{
+		array[collumn] = 0;
+		return (0);
+	}
+	return (1);
+}
+
 int	put_queen(int collumn, int *array, int *counter)
 {
-	// Caso base >> Ultima solucao... desnecessario ???
-	if (array[0] == 10)
-		return (0);
-	// Caso recursivo
 	while (array[collumn] < 10)
 	{
 		while (position_valid(collumn, array) == 0)
 		{
-			if (array[collumn] == 9) // Se na ultima linha
+			if (end_of_collumn(collumn, array) == 0)
 				return (0);
-			else // Verifica que 
-				array[collumn]++;
+			array[collumn]++;
 		}
 		if (collumn == 9)
 		{
 			success(array, counter);
-			return (1);
+			if (end_of_collumn(collumn, array) == 0)
+				return (0);
+			array[collumn]++;
+			return (0);
 		}
 		else
 		{
-			array[collumn + 1] = 0;
 			put_queen(collumn + 1, array, counter);
-			if (array[collumn] == 9)
+			if (end_of_collumn(collumn, array) == 0)
 				return (0);
-			array[collumn]++; // ?????
+			array[collumn]++;
 		}
 	}
+	return (1);
 }
-
 
 int	ft_ten_queens_puzzle(void)
 {
-	int array[10] = {0, 2, 5, 7, 9, 4, 8, 1, 0, 0};
-	int counter;
-	
-//	array[0] = 0;
+	int	array[10];
+	int	i;
+	int	counter;
+
+	i = 0;
+	while (i < 10)
+	{
+		array[i] = 0;
+		i++;
+	}
 	counter = 0;
 	put_queen(0, array, &counter);
 	return (counter);
 }
 
+/*
 int main (void)
 {
 	ft_ten_queens_puzzle();
 }
+*/
