dfc2c54a81ec7245ebaf3a885ec47848445b7608
Miguel <miguel.srmv@gmail.com>
2023-03-28 12:18:29 +0100

    end of morning

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -3,13 +3,13 @@
 #                                                         :::      ::::::::    #
 #    libft_creator.sh                                   :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: mde-sa-- <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/28 11:17:40 by mde-sa--          #+#    #+#              #
-#    Updated: 2023/03/28 11:23:05 by mde-sa--         ###   ########.fr        #
+#    Updated: 2023/03/28 12:17:17 by mde-sa--         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-gcc -Wall -Wextra -Werror -c libft_creator.o ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
-ar rcs libft_creator.sh libft_creator.o
+gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
 rm *.o
